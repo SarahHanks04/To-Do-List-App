185af057f23c61fcf7309c287ad389a9ca34356d
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-15 22:02:19 +0100

    updated

---
+++ src/component/ToDO.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+import todo_icon from '../assets/todo_icon.png' 
+import TodoItems from './ToDoItems'
+
+const ToDO = () => {
+    const inputRef = useRef();
+
+  const add = () => {
+        const inputText = inputRef.current.value;
+        console.log(inputText);
+  }
+
+
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+
+        {/* TITLE */}
+      <div className='flex items-center mt-7 gap-2'>
+        <img className='w-8' src={todo_icon} alt="" />
+        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+      </div>
+
+        {/* INPUT BOX */}
+      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+
+        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 ' type="text" placeholder='Add your task' />
+
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+
+      </div>
+
+      {/* TODO LIST */}
+      <div>
+        <TodoItems text="Learn Coding" />
+        <TodoItems text="Learn Coding From Sarah" />
+      </div>
+
+    </div>
+  )
+}
+
+export default ToDO
