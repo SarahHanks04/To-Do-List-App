3d05af1519c7ff505679288d50f3f02e06c582bb
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-15 23:37:50 +0100

    updated

--- src/component/ToDoItems.jsx
+++ src/component/ToDoItems.jsx
@@ -7,8 +7,8 @@ const TodoItems = ({text, id, isComplete, deleteTodo}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
 
-        <div onClick={() => {ToggleEvent(id)}} className='flex flex-1 items-center cursor-pointer'>
-        
+        <div onClick={() => {Toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+
             <img src={tick} alt="" className='w-7' />
             <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
 
