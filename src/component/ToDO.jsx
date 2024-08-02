04769c1cd01add4839c3e6d068a59e1c548606f0
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-02 01:51:36 +0100

    updated

--- src/component/ToDO.jsx
+++ src/component/ToDO.jsx
@@ -53,7 +53,7 @@ const ToDo = () => {
         {/* TITLE */}
       <div className='flex items-center mt-7 gap-2'>
         <img className='w-8' src={todo_icon} alt="" />
-        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+        <h1 className='text-3xl font-semibold'>My Daily To-Do</h1>
       </div>
 
         {/* INPUT BOX */}
@@ -61,7 +61,7 @@ const ToDo = () => {
 
         <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 ' type="text" placeholder='Add your task' />
 
-        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <button onClick={add} className='border-none rounded-full bg-[#E6B4] w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
 
       </div>
 
